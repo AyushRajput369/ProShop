cecd64990b8cb43f48ee3704aff81712dfedc114
AyushRajput369 <aayyuusshh8@gmail.com>
2021-11-29 00:48:33 +0530

    productScreen implemented

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { useNavigate } from 'react-router';
 
 const Header = () => {
+  let navigate = useNavigate();
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <Navbar.Brand onClick={()=> navigate('/')} style={{cursor:'pointer'}}>ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
+              <Nav.Link onClick={()=> navigate('/cart')}>
                 <i className='fas fa-shopping-cart'></i> Cart
               </Nav.Link>
-              <Nav.Link href='/login'>
+              <Nav.Link onClick={()=> navigate('/login')}>
                 <i className='fas fa-user'></i> Sign In
               </Nav.Link>
             </Nav>
